6266ea4ada748d78c8fa4d4c543bb1776eb3347a
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2021-11-09 12:41:35 -0300

    update num batch

--- testing.py
+++ testing.py
@@ -12,14 +12,16 @@ cuda = torch.device('cuda:0')
 
 train_iter = AG_NEWS(split='train')
 
-dataloader = DataLoader(train_iter, batch_size=8, shuffle=False)
+dataloader = DataLoader(train_iter, batch_size=256, shuffle=False)
 
 
 with torch.cuda.device(0):
 
 
     isn = IncrementalSkipGram(tokenizer=word_tokenize, device=cuda)
-
+    i = 0
     for batch in dataloader:
         isn.fit(batch[1])
+        print(f'batch num {i}')
+        i += 1
     
